c4c4af644fc20ed375b166f66c99c7959c1e68f6
Likhit Garimella <likhitgmella@gmail.com>
2020-12-03 12:57:38 +0530

    complete inital setup

--- iMorph3d/ViewController.swift
+++ iMorph3d/ViewController.swift
@@ -11,9 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
     }
-
-
-}
-
+    
+}   // #20
