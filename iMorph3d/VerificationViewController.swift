b5695c763723dc95e07a59a8a34f82eb784b5514
Likhit Garimella <likhitgmella@gmail.com>
2020-12-03 14:27:18 +0530

    btn, nav ctrlr

---
+++ iMorph3d/VerificationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  VerificationViewController.swift
+//  iMorph3d
+//
+//  Created by Likhit Garimella on 03/12/20.
+//
+
+import UIKit
+
+class VerificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // remove title for left bar button item
+        navigationController?.navigationBar.topItem?.title = ""
+        
+    }
+    
+}   // #21
