3d73c23a59e1052d4414d04beacd550a25562db7
Likhit Garimella <likhitgmella@gmail.com>
2020-12-03 15:45:31 +0530

    otp fields done

--- iMorph3d/VerificationViewController.swift
+++ iMorph3d/VerificationViewController.swift
@@ -6,8 +6,39 @@
 //
 
 import UIKit
+import OTPFieldView
 
-class VerificationViewController: UIViewController {
+class VerificationViewController: UIViewController, OTPFieldViewDelegate {
+    
+    @IBOutlet var otpTextFieldView: OTPFieldView!
+    
+    func setupOtpView() {
+        
+        self.otpTextFieldView.fieldsCount = 4
+        self.otpTextFieldView.fieldBorderWidth = 2
+        self.otpTextFieldView.defaultBorderColor = UIColor.black
+        self.otpTextFieldView.filledBorderColor = UIColor.green
+        self.otpTextFieldView.cursorColor = UIColor.red
+        self.otpTextFieldView.displayType = .underlinedBottom
+        self.otpTextFieldView.fieldSize = 40
+        self.otpTextFieldView.separatorSpace = 8
+        self.otpTextFieldView.shouldAllowIntermediateEditing = false
+        self.otpTextFieldView.delegate = self
+        self.otpTextFieldView.initializeUI()
+    }
+    
+    func shouldBecomeFirstResponderForOTP(otpTextFieldIndex index: Int) -> Bool {
+        return true
+    }
+    
+    func enteredOTP(otp otpString: String) {
+        print("OTPString: \(otpString)")
+    }
+    
+    func hasEnteredAllOTP(hasEnteredAll hasEntered: Bool) -> Bool {
+        print("Has entered all OTP? \(hasEntered)")
+        return false
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,6 +46,8 @@ class VerificationViewController: UIViewController {
         // remove title for left bar button item
         navigationController?.navigationBar.topItem?.title = ""
         
+        setupOtpView()
+        
     }
     
-}   // #21
+}   // #54
